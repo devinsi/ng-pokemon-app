aec1253287a171f8aabc3c5500eb4459fc5c8296
Yohann <leclay.yo@gmail.com>
2023-09-10 19:32:06 +0200

    clean code

--- src/app/border-card.directive.ts
+++ src/app/border-card.directive.ts
@@ -1,21 +1,27 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[pkmBorderCard]'
 })
 export class BorderCardDirective {
+  private initialColor: string = '#f5f5f5';
+  private defaultColor: string = '#009688';
+  private defaultHeight: number = 180;
 
   constructor(private el: ElementRef) { 
-    this.setBorder('#f5f5f5');
-    this.setHeight(180);
+    this.setBorder(this.initialColor);
+    this.setHeight(this.defaultHeight);
    }
 
+  @Input('pkmBorderCard') borderColor: string; //allias
+  // @Input() pkmBorderCard: string; // without allias
+
   @HostListener('mouseenter') onMouseEnter() {
-    this.setBorder('#009688');
+    this.setBorder(this.borderColor || this.defaultColor);
   } 
 
   @HostListener('mouseleave') onMouseLeave() {
-    this.setBorder('#f5f5f5');
+    this.setBorder(this.initialColor);
   }
 
   setHeight(height: number){
