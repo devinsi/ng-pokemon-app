7dd8538e6c182007c733ebf71b7ca5b0374f58e2
Yohann <leclay.yo@gmail.com>
2023-09-12 18:22:07 +0200

    routers wiring and add view pokemonDetail

--- src/app/detail-pokemon/detail-pokemon.component.ts
+++ src/app/detail-pokemon/detail-pokemon.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { POKEMONS } from '../mock-pokemon-list';
+import { Pokemon } from '../pokemon';
 
 @Component({
   selector: 'app-detail-pokemon',
   templateUrl: './detail-pokemon.component.html',
-  styles: [
-  ]
+
 })
 export class DetailPokemonComponent {
 
+  pokemonList: Pokemon[];
+  pokemon: Pokemon |undefined;
+
+  constructor(private route: ActivatedRoute){}
+
+  ngOnInit(): void{
+    this.pokemonList = POKEMONS;
+    // Récupère l'id depuis le routeur à l'instant t
+    const pokemonId: string|null = this.route.snapshot.paramMap.get('id');
+    if(pokemonId){
+      this.pokemon = this.pokemonList.find(pokemon => pokemon.id == +pokemonId);
+    }
+    
+
+  }
 }
