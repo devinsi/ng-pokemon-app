616ea2403e45da2a3e3558ed61b1d7aaf8509040
Yohann <leclay.yo@gmail.com>
2023-09-12 16:57:43 +0200

    Modify pokemon types whith pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonTypeColorPipe
   ],
   imports: [
     BrowserModule,
